a661c5c0896df3e6ecb8e6c078bb419dbf1d8ebc
Lisa <lisakbuch@gmail.com>
2015-04-13 19:42:36 -0400

    Fix overwrite of vegApp in controller.

--- app/itemlistcontroller.js
+++ app/itemlistcontroller.js
@@ -1,21 +1,30 @@
 
-function itemListController ($scope){
-
-$scope.new_item = {};
-$scope. add_item_error = "";
-
+vegApp.controller('itemListController', function ($scope){
+	$scope.new_item = {};
+	console.log('attached');
 $scope.items = [
-			{imageurl: 'images/strawb.jpg', type: 'strawberry', name: 'Herbert Strawberry', occupation: 'dogwalker', superpower: 'power-C boost'},
-			{imageurl: 'images/blueb.jpg', type: 'blueberry', name: 'Ulysses Blueberry', occupation: 'construction worker', superpower: 'super strength' },
-			{imageurl: 'images/orange.jpg' , type: 'orange', name: 'Otto Sly Orange', occupation: 'ninja', superpower: 'serious defense' }
+			{imageurl: 'images/strawberry.png', type: 'strawberry', name: 'Herbert Strawberry', occupation: 'dogwalker', superpower: 'power-C boost'},
+			{imageurl: 'images/blueberry.png', type: 'blueberry', name: 'Ulysses Blueberry', occupation: 'construction worker', superpower: 'super strength' },
+			{imageurl: 'images/orange.png', type: 'orange', name: 'Otto Sly Orange', occupation: 'yogi', superpower: 'immuno-defense' },
+			{imageurl: 'images/carrot.png' , type: 'carrot', name: 'Dr. Claude Carrotte', occupation: 'primary care physician', superpower: 'night vision' },
+			{imageurl: 'images/apple.png' , type: 'apple', name: 'Sally Apple', occupation: 'chef', superpower: 'supreme teeth cleaning' },
+			{imageurl: 'images/broccoli.png' , type: 'broccoli', name: 'Barkley Broccoli', occupation: 'mechanic', superpower: 'unbreakable bones' },
+			{imageurl: 'images/spinach.png' , type: 'spinach', name: 'George Spinachopolous', occupation: 'rockstar', superpower: 'calcium forcefield' },
+			{imageurl: 'images/avocado.png' , type: 'avocado', name: 'Garcia Avocado', occupation: 'hairstylist', superpower: 'uber fast growing hair' },
+			{imageurl: 'images/tomato.png' , type: 'tomato', name: 'Tony Tomato', occupation: 'weather anchor', superpower: 'carcinogen-shield' }
 		];
 
-	$scope.additem = function(item){
-			$scope.items.push( item );
+
+
+
+	$scope.additem = function(new_item){
+			$scope.items.push( new_item );
 			$scope.adding_item = {};
+		$scope.newitems={};
+			$scope.add_item_error = "";
 		};
 		console.log('scope', $scope);
-	};
+	});
 
 	
 
